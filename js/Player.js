436949bdfdc7a05458f59887d1ea76eff175fa35
Reece Atkins <Reecepowered2@hotmail.com>
2025-04-18 01:19:34 -0700

    Basic Player class

--- js/Player.js
+++ js/Player.js
@@ -1,4 +1,29 @@
-"use strict";
-function main() {
-    console.log("Hello World");
+import { Direction } from "./Direction";
+export class Player {
+    constructor(startX, startY) {
+        this.x = startX;
+        this.y = startY;
+        this.inventory = new Set();
+    }
+    collectItem(item) {
+        if (!this.inventory.has(item)) {
+            this.inventory.add(item);
+        }
+    }
+    move(direction) {
+        switch (direction) {
+            case Direction.Up:
+                this.y--;
+                break;
+            case Direction.Down:
+                this.y++;
+                break;
+            case Direction.Left:
+                this.x--;
+                break;
+            case Direction.Right:
+                this.x++;
+                break;
+        }
+    }
 }
--- src/Player.ts
+++ src/Player.ts
@@ -1,6 +1,55 @@
+// Import enums ItemType and Direction
+import { ItemType } from "./ItemType";
+import { Direction } from "./Direction";
 
-// Placeholder, so folders will commit to git. 
-// Just replace comment and hello world when creating the class.
-function main() {
-    console.log("Hello World");
+/**
+ * Represents a player object for a 2D array. Manages the player's position,
+ * inventory, basic movement, and item collection.
+ */
+export class Player {
+    x: number;
+    y: number;
+    inventory: Set<ItemType>;
+
+    /**
+     * Initializes a Player with a starting position and an empty inventory.
+     * @param startX The initial x-coordinate.
+     * @param startY The initial y-coordinate.
+     */
+    constructor(startX: number, startY: number) {
+        this.x = startX;
+        this.y = startY;
+        this.inventory = new Set<ItemType>();
+    }
+
+    /**
+     * Collects a given item if not already contained in inventory.
+     * @param item The item to collect.
+     */
+    collectItem(item: ItemType): void {
+        if(!this.inventory.has(item)) {
+            this.inventory.add(item);
+        }
+    }
+
+    /**
+    * Moves the player in a given direction. Does not handle move validation here.
+    * @param direction The direction to move.
+    */
+    move(direction: Direction): void {
+        switch (direction) {
+            case Direction.Up:
+                this.y--;
+                break;
+            case Direction.Down:
+                this.y++;
+                break;
+            case Direction.Left:
+                this.x--;
+                break;
+            case Direction.Right:
+                this.x++;
+                break;
+        }
+    }
 }
