398466804705e3b9ca2e164b088991bdae042fe8
Reece Atkins <Reecepowered2@hotmail.com>
2025-04-23 23:34:30 -0700

    Removed outdated files, and restructured project folders

--- js/Direction.js
+++
@@ -1,7 +0,0 @@
-export var Direction;
-(function (Direction) {
-    Direction["Up"] = "Up";
-    Direction["Down"] = "Down";
-    Direction["Left"] = "Left";
-    Direction["Right"] = "Right";
-})(Direction || (Direction = {}));
